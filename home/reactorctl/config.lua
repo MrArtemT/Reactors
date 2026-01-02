f1934f36ed1584fbbe7604913406658bff2a5af4
MrArtemT <141519998+MrArtemT@users.noreply.github.com>
2026-01-02 20:44:09 +0300

    Update config.lua

--- home/reactorctl/config.lua
+++ home/reactorctl/config.lua
@@ -1,5 +1,4 @@
 cat <<'EOF' > /home/reactorctl/config.lua
--- /home/reactorctl/config.lua
 local cfg = {}
 
 cfg.maxReactors = 6
@@ -10,22 +9,17 @@ cfg.fluid = {
 }
 
 cfg.minFluid = 80000
-
 cfg.pollEveryReactor = 1.5
 cfg.pollEveryFluid   = 30
-
 cfg.beepOnAlarm = true
 
 cfg.colors = {
   bg     = 0x101010,
   panel  = 0x1A1A1A,
   card   = 0x161616,
-
   text   = 0xEAEAEA,
   dim    = 0xA0A0A0,
-
   border = 0x5A5A5A,
-
   red    = 0xD43C3C,
   green  = 0x2ECC71,
   blue   = 0x3498DB,
