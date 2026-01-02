debbbf73a09cf31fa0de7a5310acb5dcfe4203ba
MrArtemT <141519998+MrArtemT@users.noreply.github.com>
2026-01-02 22:14:23 +0300

    Update config.lua

--- openos/home/reactorctl/config.lua
+++ openos/home/reactorctl/config.lua
@@ -1,23 +1,21 @@
--- /home/reactorctl/config.lua
 local CFG = {
   pollEvery = 1.0,
 
-  -- Coolant alarm
-  minCoolantRatio = 0.20,   -- 20% of max
+  -- Alarm logic
+  minCoolantRatio = 0.20,     -- 20% of max
   shutdownOnLowCoolant = true,
   beepOnAlarm = true,
 
   -- UI
   ui = {
     title = "Reactor Control",
-    margin = 1,
   },
 
   -- Logging
   logFile = "/home/reactorctl/reactorctl.log",
-  logMaxKb = 256, -- rotate if file bigger than this
+  logMaxKb = 256,
 
-  -- Optional: if you want to ignore some reactors by address
+  -- Ignore reactors by address if needed
   ignore = {
     -- ["cc8b9ade"] = true,
   },
